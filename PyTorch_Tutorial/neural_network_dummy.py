201d730b10c5299d3c5e98247fc16fd8e785b500
AlecUrbany <98421122+AlecUrbany@users.noreply.github.com>
2022-11-21 19:04:11 -0800

    Update neural_network_dummy.py
    removed vestigial code

--- PyTorch_Tutorial/neural_network_dummy.py
+++ PyTorch_Tutorial/neural_network_dummy.py
@@ -26,8 +26,6 @@ trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
                                           shuffle=True, num_workers=0)
 
-testset = torchvision.datasets.CIFAR10(root='./data', train=False,
-                                       download=True, transform=transform)
 testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
                                          shuffle=False, num_workers=0)
 
@@ -77,39 +75,6 @@ net = Net()
 # ???
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
-
-# # Training the network
-# for epoch in range(2):  # loop over the dataset multiple times
-
-#     running_loss = 0.0
-#     for i, data in enumerate(trainloader, 0):
-#         # get the inputs; data is a list of [inputs, labels]
-#         inputs, labels = data
-
-#         # zero the parameter gradients
-#         optimizer.zero_grad()
-
-#         # forward + backward + optimize
-#         outputs = net(inputs)
-#         loss = criterion(outputs, labels)
-#         loss.backward()
-#         optimizer.step()
-
-#         # print statistics
-#         running_loss += loss.item()
-#         if i % 2000 == 1999:    # print every 2000 mini-batches
-#             print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
-#             running_loss = 0.0
-
-# print('Finished Training')
-
-# Save the trained model
-PATH = './cifar_net.pth'
-# torch.save(net.state_dict(), PATH)
-
-
-# Load trained model from previous run
-# net.load_state_dict(torch.load(PATH))
 
 outputs = net(images)
 
